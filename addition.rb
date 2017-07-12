e5c34da447d77d4e6c2c5653aaa083a2676b81e5
ANITA KING <ADKING>
2017-07-12 11:22:18 -0400

    simple final addition

--- addition.rb
+++ addition.rb
@@ -1,3 +1,8 @@
+# define a function called addtwo with two parameters
+def addtwo(a, b) 
+       a + b; # return a + b
+end
+ 
 print "Please enter number 1 : ";
 # get the input from the console, 
 val1 = gets;
@@ -5,4 +10,3 @@ print "Please enter number 2 : ";
 val2 = gets;
 # convert the string console inputs to_i (to_integers) and add together
 print "Answer : " , (val1.to_i + val2.to_i), "\n";
-
---
+++ addition2.rb
@@ -0,0 +1,6 @@
+def add(a, b)
+  return a + b
+end
+
+p add(8, 6)
+p add(7, 6)
