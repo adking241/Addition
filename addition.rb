100f28c8c17d953fa41016ee0721627fd414290d
ANITA KING <ADKING>
2017-07-12 11:04:22 -0400

    broken crapola

--- addition.rb
+++ addition.rb
@@ -1,3 +1,17 @@
-x = 1 + 1
-print "1 + 1 = #{x}"
+def x
+	do "1 + 1"
+	end
+def y
+	do "4 + 3"
+	end
+def z
+ do "3 + 2"
+	end
+def a 
+	do "x + y + z"
+	end
+print "x = #{x}"
+print "y = #{y}"
+print "z = #{z}"
+print "a = #{a}"
 
